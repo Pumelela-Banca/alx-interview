f78d90aefd6b3980696cced78695bfc739a2917a
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-04 15:43:09 +0200

    added function and test

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
 
 """
-
+Contains funtion that prints a list of arrays
 """
+
+
+def pascal_triangle(n):
+    """
+    It returns triangular lists
+    """
+    if n <= 0:
+        return []
+
+    all_list = [[1], [1, 1]]
+    if n == 1:
+        return [1]
+    elif n == 2:
+        return all_list
+    else:
+        on = [1, 1]
+        new = [1]
+        for x in range(2, n):
+            for y in range(len(on)):
+                if y == len(on) - 1:
+                    new.append(1)
+                    all_list.append(new)
+                    on = new
+                    new = [1]
+                    continue
+                new.append(on[y] + on[y + 1])
+        return all_list
+
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
