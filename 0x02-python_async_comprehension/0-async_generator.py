9ab651727506b686de82674e3b3a2839436f5f15
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-16 16:08:48 +0200

    yields random numbers

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+minimum operations to get to text
+"""
+import asyncio
+import random
+from typing import Iterator
+
+
+async def async_generator() -> Iterator[int]:
+    """
+    yields float
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+
+if __name__ == "__main__":
+    async def print_yielded_values():
+        result = []
+        async for i in async_generator():
+            result.append(i)
+        print(result)
+
+
+    asyncio.run(print_yielded_values())
