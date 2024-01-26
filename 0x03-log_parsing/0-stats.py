2d7a93697727796108d7d318f3b45edc1ee7d8f5
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-26 20:36:04 +0200

    shortened long line

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -19,7 +19,8 @@ def main():
     try:
         for line in sys.stdin:
             match = re.match(
-                r'^\S+ - \[\S+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$', line.strip())
+                r'^\S+ - \[\S+\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)$',
+                line.strip())
 
             if match and int(line.strip().split(" ")[-2]) in [200, 301, 400, 401, 403, 404, 405, 500]:
                 status_code, size = match.groups()
