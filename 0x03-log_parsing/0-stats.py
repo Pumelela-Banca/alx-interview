5fff51f96310524e34f6034a7b1cccaee49df093
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-27 15:57:25 +0200

    skips 0 status codes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,13 +7,14 @@ import sys
 import re
 
 
-
 def print_statistics(total_size, status_code_counts):
     """
     print format
     """
     print(f"File size: {total_size}")
     for code in sorted(status_code_counts):
+        if status_code_counts[code] <= 0:
+            continue
         print(f"{code}: {status_code_counts[code]}")
 
 
@@ -21,14 +22,19 @@ line_number = 0
 codes = [200, 301, 400, 401, 403, 404, 405, 500]
 sum_all = 0
 times = []
-n_of_counts = {}
+n_of_counts = {f"{x}": 0 for x in codes}
 try:
     for line in sys.stdin:
+        line_number += 1
+        if line_number % 10 == 0:
+            print_statistics(sum_all, n_of_counts)
+
         args = line.split()
-        if len(args) != 9:
+        if len(args) not in [8, 9]:
             continue
-        if not re.match(r"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$",
-                        args[0]):
+        if not re.match(
+                r"^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$",
+                args[0]):
             # not ip match
             continue
         spc = " "
@@ -37,27 +43,15 @@ try:
             continue
         if args[1] != "-":
             continue
-        if not re.match(r"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))",
-                        args[2].lstrip("[")):
+        if not re.match(
+                r"([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))",
+                args[2].lstrip("[")):
             # not date match
             continue
-        if int(args[7]) not in codes:
-            continue
-
         sum_all += int(args[-1])
-        times.append(int(args[-2]))
-        line_number += 1
-
-        if line_number % 10 == 0:
-            for i in sorted(times):
-                if i is None or not isinstance(i, int):
-                    continue
-                if i in n_of_counts and i in codes:
-                    n_of_counts[i] += 1
-                else:
-                    n_of_counts[i] = 1
-            print_statistics(sum_all, n_of_counts)
-            times.clear()
+        if len(args) == 8:
+            continue
+        n_of_counts[str(args[-2])] += 1
 except KeyboardInterrupt:
     print_statistics(sum_all, n_of_counts)
     sys.exit(0)
