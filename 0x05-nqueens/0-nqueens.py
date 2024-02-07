33585723a673422f3994b1aee3e5dc617eeeb525
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-08 01:50:25 +0200

    fixed pycode style

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Solving N-queens using recussion
+Solving N-queens using recursion
 """
 import sys
 
@@ -12,55 +12,60 @@ def print_solution(board):
     solution = [[i, board[i]] for i in range(len(board))]
     print(solution)
 
+
 def is_safe(board, row, col):
     """
     Checks cross and diagonals
     """
     for i in range(col):
-        if board[i] == row or board[i] - i == row - col or board[i] + i == row + col:
+        if (board[i] == row or board[i] - i ==
+                row - col or board[i] + i == row + col):
             return False
     return True
 
+
 def solve_n_queens(board, col):
     """
-    Place queens and back up  if there are conflics
+    Place queens and back up  if there are conflicts
     """
-    N = len(board)
-    if col == N:
+    n = len(board)
+    if col == n:
         print_solution(board)
         return
 
-    for i in range(N):
+    for i in range(n):
         if is_safe(board, i, col):
             board[col] = i
             solve_n_queens(board, col + 1)
+
 
 def check_args():
     """
     Checks command line
     """
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: nqueens n")
         sys.exit(1)
 
     if not sys.argv[1].isdigit():
-        print("N must be a number")
+        print("n must be a number")
         sys.exit(1)
 
-    N = int(sys.argv[1])
-    if N < 4:
-        print("N must be at least 4")
+    n = int(sys.argv[1])
+    if n < 4:
+        print("n must be at least 4")
         sys.exit(1)
+    return n
 
-    return N
 
 def main():
     """
     Main entry of module
     """
-    N = check_args()
-    board = [-1] * N
+    n = check_args()
+    board = [-1] * n
     solve_n_queens(board, 0)
+
 
 if __name__ == "__main__":
     main()
