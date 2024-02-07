40aed0f851589b84dacc2afb3b4cb3a1616f089f
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-08 01:56:44 +0200

    added correct error out puts

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -18,8 +18,8 @@ def is_safe(board, row, col):
     Checks cross and diagonals
     """
     for i in range(col):
-        if (board[i] == row or board[i] - i ==
-                row - col or board[i] + i == row + col):
+        if (board[i] == row or board[i]
+                        - i == row - col or board[i] + i == row + col):
             return False
     return True
 
@@ -44,16 +44,16 @@ def check_args():
     Checks command line
     """
     if len(sys.argv) != 2:
-        print("Usage: nqueens n")
+        print("Usage: nqueens N")
         sys.exit(1)
 
     if not sys.argv[1].isdigit():
-        print("n must be a number")
+        print("N must be a number")
         sys.exit(1)
 
     n = int(sys.argv[1])
     if n < 4:
-        print("n must be at least 4")
+        print("N must be at least 4")
         sys.exit(1)
     return n
 
