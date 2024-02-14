85ba8a6c6f6d37dfe376dcc5f2ce9d265a6970ec
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-15 01:58:46 +0200

    uses ascync functions

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,32 @@
 // Prints all  characters of a star-wars movie
 
 const request = require('request');
+const util = require('util');
 let id = Number(process.argv[2]);
-let char_names;
 
-request('https://swapi-api.alx-tools.com/api/films', (error, body) => {
+request('https://swapi-api.alx-tools.com/api/films', async (error, response, body) => {
   if (error) {
     console.error('An error occurred:', error);
+    return;
   }
-  let jsonObject = JSON.parse(body.body);
+  let jsonObject = JSON.parse(body);
+  let char_names;
   for (let names of jsonObject.results) {
     if (names.episode_id === id) {
       char_names = names.characters;
       break;
-    }}
-  for (let specific_url of char_names) {
-    request(specific_url, (error, data) => {
-      if (error) {
-        console.error('An error occurred:', error);
-      }
-      let actor = JSON.parse(data.body);
-      console.log(actor.name);
-    });
+    }
   }
+  const requestPromise = util.promisify(request);
+  async function makeRequestsInOrder(urls) {
+    for (let url of urls) {
+      try {
+        const response = await requestPromise(url);
+        console.log(JSON.parse(response.body).name);
+      } catch (error) {
+        console.error(`An error occurred while making a request to ${url}:`, error);
+      }
+    }
+  }
+  await makeRequestsInOrder(char_names);
 });
