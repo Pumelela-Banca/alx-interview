27cb754c7526f66c843eb30480e0c5ea128bac43
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-15 01:04:25 +0200

    added json.parse

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,17 +5,20 @@ const request = require('request');
 let id = Number(process.argv[2]);
 
 
-request('https://swapi-api.alx-tools.com/api/filims', (error, body) => {
+request('https://swapi-api.alx-tools.com/api/films', (error, body) => {
   if (error) {
     console.error('An error occurred:', error);
   }
-  for (let names of body.results) {
+  let jsonObject = JSON.parse(body.body);
+  for (let names of jsonObject.results) {
     if (names.episode_id == id) {
+      var char_names = names.characters;
       break;
     }}
-  for (let chars of names.characters) {
-    request(chars, (data) => {
-      console.log(data.name);
+  for (let specific_url of char_names) {
+    request(specific_url, (error, data) => {
+      let actor = JSON.parse(data.body);
+      console.log(actor.name);
     });
   }
 });
