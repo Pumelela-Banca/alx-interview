6eb008b7dea487566b45f08573903f9c1ab173ca
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-21 20:30:01 +0200

    added changed variable to small letter

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,19 +8,20 @@ def rotate_2d_matrix(matrix):
     """
     rotates 2-D matrix 90 degrees clockwise
     """
-    N = len(matrix)
-    for x in range(0, int(N / 2)):
-        for y in range(x, N - x - 1):
+    n = len(matrix)
+    for x in range(0, int(n / 2)):
+        for y in range(x, n - x - 1):
             temp = matrix[x][y]
-            matrix[x][y] = matrix[N - 1 - y][x]
-            matrix[N - 1 - y][x] = matrix[N - 1 - x][N - 1 - y]
-            matrix[N - 1 - x][N - 1 - y] = matrix[y][N - 1 - x]
-            matrix[y][N - 1 - x] = temp
+            matrix[x][y] = matrix[n - 1 - y][x]
+            matrix[n - 1 - y][x] = matrix[n - 1 - x][n - 1 - y]
+            matrix[n - 1 - x][n - 1 - y] = matrix[y][n - 1 - x]
+            matrix[y][n - 1 - x] = temp
+
 
 if __name__ == "__main__":
-    matrix = [[1, 2, 3,4],
-              [4, 5, 6,33],
-              [7, 8, 9,12]]
+    matrix = [[1, 2, 3],
+              [4, 5, 63],
+              [7, 8, 92]]
 
     rotate_2d_matrix(matrix)
     print(matrix)
