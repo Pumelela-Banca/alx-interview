108e2aec6c4b3c3e42bdf6a13026fe2aa6883468
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-07 19:53:40 +0200

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module 0-island_perimeter that contains the function island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter - returns the perimeter of the island described in grid
+    """
+    if not grid:
+        return 0
+    
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    perimeter = 0
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
