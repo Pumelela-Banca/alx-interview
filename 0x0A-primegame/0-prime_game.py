8ce52aedd8706f6a6b5fb04145f7aca24f4e91c6
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-14 08:29:11 +0200

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+"""
+prime game file
+"""
+
+
+def isWinner(x, nums):
+    """
+    prime game function.
+    """
+    def look_for(n):
+        primes = [True for i in range(n + 1)]
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [p for p in range(2, n) if primes[p]]
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for i, n in enumerate(nums, start=1):
+        primes = look_for(n)
+        turn = 0
+        while len(primes) > 0:
+            prime = primes[0]
+            primes = [p for p in primes if p % prime != 0]
+            turn += 1
+        if turn % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+        if i == x:
+            break
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
